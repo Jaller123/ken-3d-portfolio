7fd27b4deee327a31ccb3bd953b096fd661193ee
Kenatho <89846381+Kenatho@users.noreply.github.com>
2025-04-10 22:52:53 +0200

    updated hero bg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../../styles'
+import { ComputersCanvas } from './canvas'
+import herobg from '../assets/herobgretro.gif';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero-section mx-auto'
+        style={{
+        backgroundImage: `url(${herobg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100vw',
+        height: '185vw',
+        }}>Hero</section>
   )
 }
 
