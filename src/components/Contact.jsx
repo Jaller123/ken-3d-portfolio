83d48413dee4292854b3e16cf515f0479a617c41
Kenatho <89846381+Kenatho@users.noreply.github.com>
2025-06-18 14:24:33 +0200

    Added earth model to contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+import { EarthCanvas } from "./canvas";
 import styles from './Contact.module.css'
 import discord from '../assets/logos/discord.svg'
 import linkedin from '../assets/logos/linkedin.svg'
 import mail from '../assets/logos/mail.svg'
 import github from '../assets/logos/github.svg'
-
+import { slideIn } from "../utilities/motion"
 
 const Contact = () => {
   
   return (
     <div className={styles.contact}>
-      <h1>Let's Connect!</h1>
+      <div className="w-full h-[600px] sm:h-[300px] md:h-[800px] lg:h-[900px] xl:h-[700px]">
+        <EarthCanvas />
+      </div>
+      <div className={styles.contactContainer}>
+      <h1 className={styles.contactTitle}>Let's Connect!</h1>
       <div className={styles.itemsContainer}>
         <img src={discord} alt="Discord" className={styles.contactIcon} onClick={() => copyToClipboard('jaller1234')} />
         <a href="https://www.linkedin.com/in/kenath-liyanage-29013921b/" target="_blank" rel="noreferrer">
@@ -23,8 +28,9 @@ const Contact = () => {
           <img src={github} alt="GitHub" className={styles.contactIcon} />
         </a>
       </div>
+      </div>
     </div>
-  );
+  ); 
 };
 
 export default Contact
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
 const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf')
   return (
-    <div>Earth</div>
+    <primitive
+     object={earth.scene}
+     scale={1.5}
+    />
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      camera={{ position: [4, 3, 5], fov: 35 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+        autoRotate
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2} />
+        <Earth />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  )
+}
+export default EarthCanvas
